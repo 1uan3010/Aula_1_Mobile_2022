cc6ca69dc6a805a8b215f27babff51f722db03e0
1uan3010 <you@example.com>
2022-08-30 21:48:38 +0000

    calculadora

--- App.js
+++ App.js
@@ -1,117 +1,156 @@
-import React, { useState } from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
-export default function App() 
-   {
-	const [Valor1,setValor1] = useState("20");
-	const [Valor2,setValor2] = useState("3");
-	const [Resultado,setResultado] = useState("");
-	function somar ()
-	{
-		let r=parseFloat(Valor1)+parseFloat(Valor2);
+export default function App() {
+	const [valor1, setValor1] = useState("0");
+	const [valor2, setValor2] = useState("0");
+	const[ resultado, setResultado]= useState();	
+	function somar(){
+		let r = parseFloat(valor1) + parseFloat(valor2);
 		setResultado(r);
 	}
-		//form f= new form(); -> instancia
-	  return 
-		(
-			<View style={ styles.container }>
-				<View style={ styles.bloco }>
-					<Text style={ styles.titulo }> Calculo Simples </Text>
-				</View>
-
-				<View style={ styles.bloco }> 
-					 <Text style={ styles.text }> Valor: 1 </Text>
-					 <TextInput style={ styles.input } value={Valor1} onChangeText={(valor)=>setValor1(valor)}/>
-
-				</View>	
-
-				<View style={ styles.bloco }> 
-					<Text style={ styles.text }> Valor: 2 </Text>
-					<TextInput style={ styles.input } value={Valor2} onChangeText={(valor)=>setValor2(valor)}/>
-				</View>	
-
-				<View style={ styles.bloco }>
-				</View>
-
-				 <View style={ styles.bloco }>
-					 <TouchableOpacity style={ styles.botao } onPress={somar}>
-						 <Text style={ styles.TextBotao }>Somar</Text>
-					 </TouchableOpacity>
-				</View>
-
-				<View style={ styles.bloco }>			 
-					<Text style={ styles.titulo}> Resultado: {Resultado}</Text>
-				</View>
-				
-		</View>
-	 );
+	
+	function multiplicar(){
+		let r = parseFloat(valor1) * parseFloat(valor2);
+		setResultado(r);
 	}
+	
+	function subtrair(){
+		let r = parseFloat(valor1) - parseFloat(valor2);
+		setResultado(r);
+	}
+	
+	function dividir(){
+		let r = parseFloat(valor1) / parseFloat(valor2);
+		setResultado(r);
+	}
+	
+	function esponeciacao(){
+		let r = parseFloat(valor1) ** parseFloat(valor2);
+		setResultado(r);
+	}
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.tituloinicial}>Aula de APP</Text>
+		  <View style={styles.bloco}>
+			  <Text style={styles.titulo}>Calculadora Simples</Text>
+		  </View>
+		  
+		  <View style={styles.bloco}>
+			  <Text>VALOR 1: </Text>
+			  <TextInput 
+			  style={styles.input}
+			  value={valor1}
+			  onChangeText={(valor) => setValor1(valor)}
+			  keyboardType="numeric"
+			  />
+		  </View>
+		  
+		  <View style={styles.bloco}>
+			  <Text>VALOR 2: </Text>
+			  <TextInput 
+			    style={styles.input}
+			    value={valor2}
+			    onChangeText={(valor) => setValor2(valor)}
+			    keyboardType="numeric"
+			  />
+		  </View>
+		  
+		  <View style={styles.bloco2}>
+			    <TouchableOpacity 
+				style={styles.botao}
+				onPress={somar}
+				>
+		    		<Text style={styles.textoBotao}>SOMAR</Text>
+			    </TouchableOpacity>
+		  </View>
+		  
+		  <View style={styles.bloco2}>
+			    <TouchableOpacity 
+				style={styles.botao}
+				onPress={multiplicar}
+				>
+		    		<Text style={styles.textoBotao}>MULTIPLICAÇÃO</Text>
+			    </TouchableOpacity>
+		  </View>
+		  
+		  <View style={styles.bloco2}>
+			    <TouchableOpacity 
+				style={styles.botao}
+				onPress={dividir}
+				>
+		    		<Text style={styles.textoBotao}>DIVIDIR</Text>
+			    </TouchableOpacity>
+		  </View>
+		  
+		  <View style={styles.bloco2}>
+			    <TouchableOpacity 
+				style={styles.botao}
+				onPress={subtrair}
+				>
+		    		<Text style={styles.textoBotao}>SUBTRAÇÃO</Text>
+			    </TouchableOpacity>
+		  </View>
+		  
+		  <View style={styles.bloco2}>
+			    <TouchableOpacity 
+				style={styles.botao}
+				onPress={esponeciacao}
+				>
+		    		<Text style={styles.textoBotao}>ESPONENCIAÇÃO</Text>
+			    </TouchableOpacity>
+		  </View>
+		  
+		  <View style={styles.bloco}>
+				<Text style={styles.titulo}>
+					Resultado: {resultado}
+				</Text>
+		  </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
-	  
-    	flex: 1,
-	  	backgroundColor: "#DCDCDC",
-    	padding: 10,
-		border: "1px solid #000000",
-		marginTop:8,
-	    justifyContent: "center"
-	 
-  },
-	
-	bloco:{
-		
-		marginTop:5,
-		float:"center",
-		borderRadius:"8px",
-		
+    flex: 1,
     
-	},
-	
-	text: {
+  },
+  	
+	bloco:{
+		marginTop: 20,
+		width:'80%',
+		marginLeft:'10%',
 		
-		marginLeft:"20%",
-		fontSize:15,
-		fontFamily:"arial",
+	},
+	bloco2:{
+		marginTop: 15,
+		width:'80%',
+		marginLeft:'10%',
+		alignItems:'center',
 		
 	},
-	
+	titulo: {
+		fontSize: 30,
+		textAlign:'center'
+	},
 	input:{
-		
+		borderWidth:2,
 		fontSize:20,
-		border: "2px solid #000000",
-		backgroundColor:"#ffffff",
-		marginLeft:"20%",
-		marginRight:"20%",
-		borderRadius: "8px",
-		
-		
-		
-	},
-	
-	titulo:{
-		
-		justifyContent: "center",
-		textAlign: "center",
-		fontSize:25,
-		fontFamily:"impact",
-		
+		borderRadius:8,
+		textAlign: 'center'
 	},
 	botao:{
-		
-		backgroundColor: "#ffffff",
-		textAlign: "center",
-		marginLeft:"40%",
-		marginRight:"40%",
-		border: "2px solid #000000",
-		borderRadius: "8px"
-		
+		backgroundColor:'#000000',
+		width: '30%',
+		borderRadius:8
 	},
-	
-	blocoText:{
-		
-		fontSize:30,
-		fontFamily:"impact",
-		color:"#fff"
+	textoBotao:{
+		color:'#fff',
+		fontSize:15,
+		textAlign:'center'
 	},
+	tituloinicial:{
+		textAlign: 'center'
+	}
 });
